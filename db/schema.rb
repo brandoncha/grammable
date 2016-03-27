462cb2721d16a7a0ab011086a71f01bb89ccad44
brandoncha <brandoncha2005@gmail.com>
2016-03-27 20:02:32 +0000

    Switching Carrierwave to S3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160327175250) do
+ActiveRecord::Schema.define(version: 20160327195043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 20160327175250) do
     t.text     "message"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
     t.string   "myimage"
+    t.integer  "user_id"
   end
 
   add_index "grams", ["user_id"], name: "index_grams_on_user_id", using: :btree
